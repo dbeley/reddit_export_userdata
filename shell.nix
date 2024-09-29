9c5529dc135305f3b72d8b8f959c268f1aa22a15
dbeley <6568955+dbeley@users.noreply.github.com>
2024-09-29 11:05:05 +0200

    Delete pipenv files, add nix-specific files

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> { };
+
+let
+  pythonPackages = python3Packages;
+in pkgs.mkShell {
+  buildInputs = [
+    pythonPackages.python
+
+    pythonPackages.praw
+    pythonPackages.pyyaml
+  ];
+
+}
